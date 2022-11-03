d1bb0a4b4be099522155b72e44e0c56554144f7c
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2022-11-03 11:03:01 +0100

    added fixed navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,37 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Home from "./Pages/Home"
 import Projects from "./Pages/Projects"
 import AboutMe from "./Pages/AboutMe"
 import Skills from "./Pages/Skills"
 import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
 
 function App() {
+  const [shownav, setShownav] = useState(false)
+  const wrapRef = useRef(null)
+  function setFixedNav() {
+    const navbar = document.getElementById("navbar")
+    const navHeight = navbar.getBoundingClientRect().height
+    const scrollHeight = window.scrollY
+    if (scrollHeight > navHeight) {
+      setShownav(true)
+    } else {
+      setShownav(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", setFixedNav)
+    return () => window.removeEventListener("scroll", setFixedNav)
+  })
   return (
-    <div id='wrapper'>
-      <Navbar />
+    <div id='wrapper' ref={wrapRef}>
+      <Navbar actions={{ shownav }} />
       <Home />
       <AboutMe />
       <Skills />
-
       <Projects />
+      <Footer />
     </div>
   )
 }
--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -1,12 +1,34 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import { FaBars, FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa"
 
-function Navbar() {
+function Navbar({ actions }) {
   const linksRef = useRef(null)
   const linksContainer = useRef(null)
+  const { shownav } = actions
 
+  function setNavigation(e) {
+    e.preventDefault()
+    const scrollheight = window.scrollY
+    const navbar = document.getElementById("navbar")
+    const navHeight = navbar.getBoundingClientRect().height
+    const id = e.target.getAttribute("href").slice(1)
+    const element = document.getElementById(id)
+    let position = element.offsetTop - navHeight
+    if (!shownav) {
+      position = position - navHeight
+      window.scrollTo({
+        left: 0,
+        top: position,
+      })
+    } else {
+      window.scrollTo({
+        left: 0,
+        top: position,
+      })
+    }
+  }
   return (
-    <div className='navbar'>
+    <div id='navbar' className={shownav ? "fixed-nav" : "navbar"}>
       <div className='logo'>
         <FaAngleDoubleLeft />
         <span>GBN</span>
@@ -15,16 +37,24 @@ function Navbar() {
       <div className='links-container' ref={linksContainer}>
         <ul className='links' ref={linksRef}>
           <li>
-            <a href='#home'>Home</a>
+            <a href='#home' onClick={setNavigation}>
+              Home
+            </a>
           </li>
           <li>
-            <a href='#about'>About Me</a>
+            <a href='#about' onClick={setNavigation}>
+              About Me
+            </a>
           </li>
           <li>
-            <a href='#skills'>Skills</a>
+            <a href='#skills' onClick={setNavigation}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href='#projects'>Projects</a>
+            <a href='#projects' onClick={setNavigation}>
+              Projects
+            </a>
           </li>
         </ul>
       </div>
