ce0bfd2120e0b7c41f3cf1ce8823ab1c5391d6b9
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2022-12-10 12:48:05 +0100

    removed github and linkedIn icons from footer section

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -5,15 +5,6 @@ import { SiGmail } from "react-icons/si"
 const Footer = () => {
   return (
     <div id='footer'>
-      <div >
-        <a href='https://github.com/GreatnessBarthNathan'>
-          <FaGithub />
-        </a>
-        <a href='https://linkedin.com/in/bartholomew-ekemezie-b7873691'>
-          <FaLinkedin />
-        </a>
-      </div>
-
       <div>
         <h3>Contact:</h3>
         <p>Telephone: +234 816 270 0336</p>
